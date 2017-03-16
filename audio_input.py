dfae5f8b57759905e70011d34a53145e3803e368
joey.cheng <jemicheng@gmail.com>
2017-03-16 12:46:37 +0800

    add mutual exclusion

--- audio_input.py
+++ audio_input.py
@@ -5,6 +5,9 @@ import numpy as np
 import sys
 import tensorflow as tf
 import time
+import threading
+
+mutex = threading.Lock()
 
 class RingBuffer:
     """ class that implements a not-yet-full buffer """
@@ -34,7 +37,7 @@ class RingBuffer:
         """ Return a list of elements from the oldest to the newest. """
         return self.data
 
-ringBuffer = RingBuffer(3 * 2048)
+ringBuffer = RingBuffer(100 * 4096)
 
 def buf_to_float(x, n_bytes=2, dtype=np.float32):
     # Invert the scale of the data
@@ -56,6 +59,7 @@ def extract_feature(data,sr=8000):
     mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
     contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate,fmin=60.0).T,axis=0)
     tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+    #rmse=np.mean(librosa.feature.rmse(y=X,frame_length=128).T,axis=0)
     return mfccs,chroma,mel,contrast,tonnetz
 """
 def callback(in_data, frame_count, time_info, flag):
@@ -103,33 +107,35 @@ def callback(in_data, frame_count, time_info, flag):
 def callback(in_data, frame_count, time_info, flag):
     #print(frame_count)
     #print(len(in_data))
+    mutex.acquire()
     ringBuffer.append(in_data)
+    mutex.release()
     return None, pyaudio.paContinue
 
 training_epochs = 5000
 n_dim = 193#features.shape[1]
 n_classes =10
-n_hidden_units_one = 193 
+n_hidden_units_one = 193
 n_hidden_units_two = 386
 sd = 1 / np.sqrt(n_dim)
 learning_rate = 0.01
 
 X = tf.placeholder(tf.float32,[None,n_dim])
     
-W_1 = tf.Variable(tf.random_normal([n_dim,n_hidden_units_one], mean = 0, stddev=sd),name="Weight_1")#193,280
+W_1 = tf.Variable(tf.random_normal([n_dim,n_hidden_units_one], mean = 0, stddev=sd),name="Weight_1")
 b_1 = tf.Variable(tf.random_normal([n_hidden_units_one], mean = 0, stddev=sd),name="bias_1")
 h_1 = tf.nn.tanh(tf.matmul(X,W_1) + b_1)
 
-W_2 = tf.Variable(tf.random_normal([n_hidden_units_one,n_hidden_units_two], mean = 0, stddev=sd),name="Weight_2")#280,300
+W_2 = tf.Variable(tf.random_normal([n_hidden_units_one,n_hidden_units_two], mean = 0, stddev=sd),name="Weight_2")
 b_2 = tf.Variable(tf.random_normal([n_hidden_units_two], mean = 0, stddev=sd),name="bias_2")
 h_2 = tf.nn.sigmoid(tf.matmul(h_1,W_2) + b_2)
 
-W = tf.Variable(tf.random_normal([n_hidden_units_two,n_classes], mean = 0, stddev=sd),name="W")#300,7
+W = tf.Variable(tf.random_normal([n_hidden_units_two,n_classes], mean = 0, stddev=sd),name="W")
 b = tf.Variable(tf.random_normal([n_classes], mean = 0, stddev=sd),name="b")
 y_ = tf.nn.softmax(tf.matmul(h_2,W) + b)
 saver =  tf.train.Saver()
 
-CHUNK = 1024
+CHUNK = 2048
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 8000
@@ -147,12 +153,15 @@ stream = p.open(format=FORMAT,
 
 
 stream.start_stream()
+
 with tf.Session() as sess: 
     saver.restore(sess, "D:\\source\\python\\UrbanSound8K\\pkl\\audio.ckpt")
     print("restore session")
     while stream.is_active():   
         time.sleep(0.25)
+        mutex.acquire()
         in_data=np.array(ringBuffer.get())
+        mutex.release()
         #print(in_data)
         x=buf_to_float(in_data)
         print(x)
@@ -164,8 +173,8 @@ with tf.Session() as sess:
         print('execute prediction')          
         y_pred = sess.run(tf.argmax(y_,1),feed_dict={X: test_x})
         print(y_pred) 
+
     
-#stream.stop_stream()
 stream.close()
 p.terminate()
 
